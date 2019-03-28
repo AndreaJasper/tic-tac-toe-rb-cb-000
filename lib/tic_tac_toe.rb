9813870e8454a81a130f0610490c9c327d64a821
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-03-28 15:39:31 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -140,9 +140,10 @@ def play(board)
   until over?(board) == true
     turn(board)
   end
-    if won?(board)
-      puts "Congratulations #{winner}"
-    elsif draw?(board)
-      puts "Cat's Game!"
-    end
+
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cats Game!"
+  end
 end
